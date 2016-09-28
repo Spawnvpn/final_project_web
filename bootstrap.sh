de67bb1a7b1fb7e3927cd8459bbb094144ebc55e
bogdan <spawnvpn@gmail.com>
2016-09-28 21:55:56 +0300

    Configure scripts for deploying project on remote machine via nginx.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,11 @@
 
 apt-get update
 apt-get install -y python-pip
-apt-get install -y build-essential libssl-dev libffi-dev python-dev libffi-dev  libpq-dev redis server
+sudo easy_install supervisor
+apt-get install -y build-essential libssl-dev libffi-dev python-dev libffi-dev  libpq-dev redis-server postgresql postgresql-contrib nginx uwsgi
 pip install virtualenv
+pip install supervisor
+
 
 #apt-get install -y apache2
 #if ! [ -L /var/www ]; then
@@ -13,7 +16,15 @@ pip install virtualenv
 
 python2 -m virtualenv /vagrant/.env
 /vagrant/.env/bin/pip install -r /vagrant/requirements.txt
-#/vagrant/.env/bin/python /vagrant/manage.py makemigrations
-#/vagrant/.env/bin/python /vagrant/manage.py migrate
+/vagrant/.env/bin/python /vagrant/manage.py collectstatic
+/usr/bin/python3 -m virtualenv /vagrant/autobahn_proj/.env
+/vagrant/autobahn_proj/.env/bin/pip install -r /vagrant/autobahn_proj/requirements.txt
+sudo -i -u postgres
+sudo psql -f postgre_conf.sql
+sudo /etc/supervisord -c /vagrant/supervisord.conf
+sudo /etc/init.d/nginx restart
+
+/vagrant/.env/bin/python /vagrant/manage.py makemigrations
+/vagrant/.env/bin/python /vagrant/manage.py migrate
 
 
